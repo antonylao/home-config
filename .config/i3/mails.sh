ca21d8ca11d32b1037ab6b4d1f7a2ff7d666f6a8
Antony <antony.lao@laposte.net>
2025-05-27 20:27:33 +0200

    change removing notification from noice keybindings to kn

---
+++ .config/i3/mails.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+firefox -P Email --class Email -new-tab -url https://www.laposte.net/accueil \
+  -new-tab -url https://outlook.live.com/mail/0/?prompt=select_account \
+  -new-tab -url https://www.gmail.com 
+
+exit 0
